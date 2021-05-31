472dcfce3c5ed841937041a4413c87fc4e3a17c0
Ilya-Grin <mrpsychopath2110@gmail.com>
2021-05-31 15:06:53 +0300

    31.05.2021

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,9 @@
 const burger = document.querySelector(".burger");
 const menu = document.querySelector(".menu");
+const menuRegistration = document.querySelector(".menu-registration");
+const burgerWrapper = document.querySelector(".burger-wrapper");
 
 function burgerClick() {
 	menu.classList.toggle("enable");
+	menuRegistration.classList.toggle("enable-registration");
 }
